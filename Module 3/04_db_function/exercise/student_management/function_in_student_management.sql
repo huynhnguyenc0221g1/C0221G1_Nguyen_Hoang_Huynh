9380e1ecf9352fafd314ea7e5d6763985d62afc3
Huynh Nguyen <huynhnguyen@Huynhs-MacBook-Pro.local>
2021-05-17 10:22:28 +0700

    update exercise session 04

---
+++ Module 3/04_db_function/exercise/student_management/function_in_student_management.sql
@@ -0,0 +1,92 @@
+CREATE DATABASE student_management;
+USE student_management;
+CREATE TABLE class
+(
+    class_id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    class_name VARCHAR(60) NOT NULL,
+    start_date DATETIME    NOT NULL,
+    status    BIT
+);
+
+CREATE TABLE student
+(
+    student_id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    student_name VARCHAR(30) NOT NULL,
+    address     VARCHAR(50),
+    phone       VARCHAR(20),
+    status      BIT,
+    class_id     INT         NOT NULL,
+    FOREIGN KEY (class_id) REFERENCES class (class_id)
+);
+CREATE TABLE `subject`
+(
+    subject_id   INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    subject_name VARCHAR(30) NOT NULL,
+    credit  TINYINT     NOT NULL DEFAULT 1 CHECK ( credit >= 1 ),
+    `status`  BIT                  DEFAULT 1
+);
+CREATE TABLE mark
+(
+    mark_id    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    subject_id     INT NOT NULL,
+    student_id INT NOT NULL,
+    mark      FLOAT   DEFAULT 0 CHECK ( mark BETWEEN 0 AND 100),
+    exam_times TINYINT DEFAULT 1,
+    UNIQUE (subject_id, student_id),
+    FOREIGN KEY (subject_id) REFERENCES subject (subject_id),
+    FOREIGN KEY (student_id) REFERENCES student (student_id)
+);
+
+USE student_management;
+INSERT INTO class
+VALUES (1, 'A1', '2008-12-20', 1);
+INSERT INTO class
+VALUES (2, 'A2', '2008-12-22', 1);
+INSERT INTO class
+VALUES (3, 'B3', current_date, 0);
+
+INSERT INTO student (student_name, address, phone, status, class_id)
+VALUES ('Hung', 'Ha Noi', '0912113113', 1, 1);
+INSERT INTO student (student_name, address, status, class_id)
+VALUES ('Hoa', 'Hai phong', 1, 1);
+INSERT INTO student (student_name, address, phone, status, class_id)
+VALUES ('Manh', 'HCM', '0123123123', 0, 2);
+
+INSERT INTO subject
+VALUES (1, 'CF', 5, 1),
+       (2, 'C', 6, 1),
+       (3, 'HDJ', 5, 1),
+       (4, 'RDBMS', 10, 1);
+       
+INSERT INTO subject
+VALUES (5, 'CF', 4, 1);
+
+INSERT INTO mark (subject_id, student_id, mark, exam_times)
+VALUES (1, 1, 8, 1),
+       (1, 2, 10, 2),
+       (2, 1, 12, 1);
+       
+INSERT INTO mark (subject_id, student_id, mark, exam_times)
+VALUES (1, 3, 8, 1);
+
+-- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
+select * , max(credit)
+from `subject` 
+group by `subject`.subject_id
+ having max(credit) >= ALL (select max(credit) from `subject`);
+
+-- Hiển thị các thông tin môn học có điểm thi lớn nhất.
+select `subject`.subject_name,`subject`.credit,`subject`.`status`, max(mark.mark)
+from `subject`
+join mark 
+on `subject`.subject_id = mark.subject_id
+group by `subject`.subject_id
+having max(mark.mark)>= all (select max(mark.mark) from mark);
+
+-- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần.
+select student.student_name,student.address,student.phone , avg(mark.mark) as average_point
+from student
+join mark 
+on student.student_id = mark.student_id
+group by student.student_id
+order by avg(mark.mark) DESC;
